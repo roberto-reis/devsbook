716e2369ce9119612c29693f0c830f61989eb201
Roberto <tekinforroberto@gmail.com>
2021-07-16 17:05:35 -0300

    feat: fanalizado a feed-editor o front e back

--- src/routes.php
+++ src/routes.php
@@ -8,6 +8,7 @@ $router->get('/login', 'LoginController@signin');
 $router->post('/login', 'LoginController@signinAction');
 $router->get('/cadastro', 'LoginController@signup');
 $router->post('/cadastro', 'LoginController@signupAction');
+$router->post('/post/new', 'PostController@new');
 
 // $router->get('/pesquisa');
 // $router->get('/perfil');
