6a64bea4a796bcf34849ef00845892696f36045c
Roberto <tekinforroberto@gmail.com>
2021-07-19 16:26:43 -0300

    feat: Finalizado o partil do perfil

--- src/views/pages/profile_photos.php
+++ src/views/pages/profile_photos.php
@@ -2,47 +2,15 @@
 
 <section class="container main">  
 
-    <?php echo $render('sidebar', ['activeMenu'=>'friends']); ?>
+    <?php echo $render('sidebar', ['activeMenu'=>'photos']); ?>
 
     <section class="feed">
 
-        <div class="row">
-            <div class="box flex-1 border-top-flat">
-                <div class="box-body">
-                    <div class="profile-cover" style="background-image: url('<?php echo $base; ?>/media/covers/<?php echo $user->cover; ?>');"></div>
-                    <div class="profile-info m-20 row">
-                        <div class="profile-info-avatar">
-                            <img src="<?php echo $base; ?>/media/avatars/<?php echo $user->avatar; ?>" />
-                        </div>
-                        <div class="profile-info-name">
-                            <div class="profile-info-name-text"><?php echo $user->name; ?></div>
-                            <div class="profile-info-location"><?php echo $user->city; ?></div>
-                        </div>
-                        <div class="profile-info-data row">
-
-                            <?php if($user->id != $loggedUser->id): ?>
-                                <div class="profile-info-item m-width-20">
-                                    <a href="<?php echo $base; ?>/perfil/<?php echo $user->id; ?>/follow" class="button"><?php echo (!$isFollowing) ? 'Seguir' : 'Deixa de Seguir'; ?></a>
-                                </div>
-                            <?php endif; ?>
-
-                            <div class="profile-info-item m-width-20">
-                                <div class="profile-info-item-n"><?php echo count($user->followers); ?></div>
-                                <div class="profile-info-item-s">Seguidores</div>
-                            </div>
-                            <div class="profile-info-item m-width-20">
-                                <div class="profile-info-item-n"><?php echo count($user->following); ?></div>
-                                <div class="profile-info-item-s">Seguindo</div>
-                            </div>
-                            <div class="profile-info-item m-width-20">
-                                <div class="profile-info-item-n"><?php echo count($user->photos); ?></div>
-                                <div class="profile-info-item-s">Fotos</div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
+        <?= $render('perfil-header', [
+            'loggedUser' => $loggedUser,
+            'user' => $user,
+            'isFollowing' => $isFollowing
+        ]); ?>
         
         <div class="row">
 
